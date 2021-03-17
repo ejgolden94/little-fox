7721c37fdb74a50f24f63f8a2b95078bac89b977
Ellyn Golden <ejgolden94@gmail.com>
2021-03-16 21:58:13 -0400

    flavors seed route is working

--- controllers/flavors.js
+++ controllers/flavors.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router()
+const Flavors = require('../models/flavors');
+const flavorsSeed = require('../models/flavorsSeed');
 
 //ROUTES 
 
@@ -8,7 +10,16 @@ router.get('/',(req,res)=>{
     res.render('index.ejs')
 })
 
-
+// Flavors Seed Route 
+router.get('/seed',(req,res)=>{
+    Flavors.create(flavorsSeed, (err, newFlavors)=>{
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(newFlavors);
+        }
+    })
+})
 
 
 module.exports = router
---
+++ models/flavorsSeed.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        flavor: 'This is Not A Beer',
+        description: 'Bourbon barrel aged stout chocolate cream + stout caramel + peanut brittle',
+        price: 5, 
+        available: true,
+        allergens: ['dairy','peanuts'], 
+        img: 'this_is_not_a_beer.jpg'
+    }
+]
