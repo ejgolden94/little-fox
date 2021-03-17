861888b12d4141320a4fc4481956a0e230c5c619
Ellyn Golden <ejgolden94@gmail.com>
2021-03-16 21:41:19 -0400

    mongo connection established

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 require('dotenv').config()
 const express = require('express');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT
 const app = express()
 const methodOverride = require('method-override');
 
-/// MODLES
+/// MODELS
 const Flavors = require('./models/flavors');
+
+/// DATABASE
+const mongoURI = process.env.MONGODBURI
+const db = mongoose.connection
+mongoose.connect(mongoURI, {
+    useFindAndModify: false,
+    useCreateIndex: true, 
+    useUnifiedTopology: true
+})
+
+db.on('error',(err)=>{console.log('ERROR! ' + err.message)})
+db.on('connected',()=>{console.log('mongo is connected')})
+db.on('disconnected',()=>{console.log('mongo is disconnected')})
 
 /// MIDDLEWARE
 app.use(express.json())
@@ -14,6 +28,7 @@ app.use(express.static('public'))
 
 /// CONTROLLERS
 const flavorsController = require('./controllers/flavors');
+const { Mongoose } = require('mongoose');
 app.use('/flavors',flavorsController)
 
 /// LISTENER
