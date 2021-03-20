f667d53175e3a9614879780e4758a5ea74a2d775
Ellyn Golden <ejgolden94@gmail.com>
2021-03-20 10:14:32 -0400

    create user route is working and hashing pw

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const router = express.Router()
+const bcrypt = require('bcrypt');
 const User = require('../models/users');
 
 // ROUTES
 
 // New User Route
-
 router.get('/new', (req,res)=>{
     res.render('users/new.ejs',{
         title: 'New User'
+    })
+})
+
+// Creat User Route
+router.post('/', (req, res)=>{
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+
+    User.create(req.body, (err, newUser)=>{
+        if (err) {
+            if(err.code === 11000){
+                res.send("That username is not available :(")
+            }
+        } else {
+            console.log(newUser);
+            res.redirect('/flavors')
+        }
     })
 })
 
