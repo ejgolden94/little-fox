8b435f00f604e2a91f3d41e23a032199a2c544fd
Ellyn Golden <ejgolden94@gmail.com>
2021-03-25 22:10:40 -0400

    update seed route and media query

--- models/flavorsSeed.js
+++ models/flavorsSeed.js
@@ -88,5 +88,105 @@ module.exports = [
         plantBased: false,
         allergens: ['dairy'], 
         img: null
+    },
+    {
+        flavor: 'Blueberries',
+        description: 'super yummy bowl of bloobs',
+        price: 3, 
+        available: true,
+        glutenFree: true,
+        plantBased: true,
+        allergens: [], 
+        img: null
+    },
+    {
+        flavor: 'Let Them Eat Cake Taco',
+        description: 'vanilla cream + sugar cookie crumble + cake batter + rainbow sprinkle shell + white chocolate + rainbow sprinkles.',
+        price: 5.50, 
+        available: true,
+        glutenFree: false,
+        plantBased: true,
+        allergens: ['dairy'], 
+        img: null
+    },
+    {
+        flavor: 'Death Before Decaf Taco',
+        description: 'cold-brewed coffee cream + oat cookie crumble + chocolate waffle shell + chocolate + cocoa powder',
+        price: 5.50, 
+        available: true,
+        glutenFree: false,
+        plantBased: false,
+        allergens: ['dairy'], 
+        img: null
+    },
+    {
+        flavor: 'Once Upon a Time in Hollywood Taco',
+        description: 'sweet corn cream + honeycomb + caramel + sweet cream waffle shell + chocolate + caramel drizzle',
+        price: 5.50, 
+        available: true,
+        glutenFree: true,
+        plantBased: true,
+        allergens: ['dairy'], 
+        img: null
+    },
+    {
+        flavor: 'Easy Street Taco',
+        description: 'elderflower lemon cream + raspberry compote + raspberry waffle shell + lemon chocolate + raspberry dust',
+        price: 5.50, 
+        available: true,
+        glutenFree: true,
+        plantBased: true,
+        allergens: ['dairy'], 
+        img: null
+    },
+    {
+        flavor: 'Cooler Than You Think Taco',
+        description: 'mint chocolate cream + brownie batter + mint crystals + mint waffle shell + chocolate + mint drizzle',
+        price: 5.50, 
+        available: true,
+        glutenFree: false,
+        plantBased: false,
+        allergens: ['dairy'], 
+        img: null
+    },
+    {
+        flavor: 'This is Not A Beer Taco',
+        description: 'stout chocolate cream + peanut brittle + stout caramel swirl + peanut butter waffle shell + peanut brittle bits',
+        price: 5.50, 
+        available: true,
+        glutenFree: false,
+        plantBased: false,
+        allergens: ['dairy','peanuts'], 
+        img: null
+    },
+    {
+        flavor: 'Things \'n\' Stuff Taco',
+        description: 'salted pretzel cream + peanut butter + chocolate chunks + pretzel bits + peanut butter waffle shell + pretzel crumbs',
+        price: 5.50, 
+        available: true,
+        glutenFree: false,
+        plantBased: false,
+        allergens: ['dairy','peanuts','treenuts'], 
+        img: null
+    },
+    {
+        flavor: 'Things \'n\' Stuff Pint',
+        description: 'A pint of our things \'n\' stuff ice cream. salted pretzel cream + peanut butter + chocolate chunks + pretzel bits + peanut butter waffle shell + pretzel crumbs',
+        price: 5.50, 
+        available: true,
+        glutenFree: false,
+        plantBased: false,
+        allergens: ['dairy','peanuts','treenuts'], 
+        img: null
+    },
+    {
+        flavor: 'Death Before Decaf Pint',
+        description: 'A pint of our death before decaf ice cream. a pint of our death before decaf ice cream: cold-brewed coffee cream + oat cookie crumble',
+        price: 5.50, 
+        available: true,
+        glutenFree: false,
+        plantBased: false,
+        allergens: ['dairy'], 
+        img: null
     }
 ]
